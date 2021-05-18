a76386c568aaac6fd98d3945a04cb056a252248f
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-05-18 09:59:57 +0530

    ADDED: Calculating wages with thhe help of array

--- empWage.js
+++ empWage.js
@@ -6,13 +6,14 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR =20;
 const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 100;
+const MAX_HRS_IN_MONTH = 160;
 
 //variables
 let empWage = 0;
 let empHrs = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 
 //using function to check emp working hours for a day
 function getWorkingHours(empCheck){
@@ -27,14 +28,21 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+
+//function for daily wage
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
 // Expression for employee wage for a month
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     empCheck = Math.floor(Math.random()*10)%3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
 
-empWage = totalEmpHrs * WAGE_PER_HOUR;
+empWage = calcDailyWage(totalEmpHrs);
 console.log("Total Days: " +totalWorkingDays+ " Total Hrs: "+totalEmpHrs+ " Emp Wage: "+empWage);
 
 
